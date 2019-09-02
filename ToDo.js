d48541d7ce62b33c8f89d1b674cc013bad1f812e
Ioannis Sanos <isan0002@student.monash.edu>
2019-09-02 21:00:11 +1000

    week 6 lab

--- ToDo.js
+++ ToDo.js
@@ -1,9 +1,10 @@
 let express = require('express');
 let app = express();
 
+let mongodb = require("mongodb");
 let bodyParser = require('body-parser');
 
-let db = [];
+let db = null;
 
 let viewsPath = __dirname + "/views/";
 
@@ -17,6 +18,19 @@ app.use(bodyParser.urlencoded({
     extended: false
 }));
 
+const MongoClient = mongodb.MongoClient;
+const url = "mongodb://localhost:27017/";
+
+MongoClient.connect(url, { useNewUrlParser: true },
+    function (err, client) {
+        if (err) {
+            console.log("Err  ", err);
+        } else {
+            console.log("Connected successfully to server");
+            db = client.db("fit2095db");
+        }
+    });
+
 app.get("/", function(req, res) {
     let fileName = viewsPath + "/index.html";
     res.sendFile(fileName);
@@ -27,15 +41,46 @@ app.get("/addNewTask", function(req, res){
     res.sendFile(fileName);
 });
 
-app.get("/getAllTasks", function(req,res){
-    let fileName = viewsPath + "/getAllTasks.html";
-    res.render(fileName, {tasks: db});
+app.post('/addNewTask', function (req, res) {
+    let taskDetails = req.body;
+    db.collection('week6').insertOne({ taskName: taskDetails.taskName, dueDate: taskDetails.dueDate, taskStatus: taskDetails.taskStatus, description: taskDetails.description });
+    res.redirect('/getAllTasks'); 
 });
 
-app.post("/addNewTask", function(req,res){
-        console.log(req.body);
-        db.push(req.body);
-        res.redirect("/getAllTasks");
+app.get("/getAllTasks", function(req,res){
+    db.collection('week6').find({}).toArray(function (err, data) {
+        res.render('getAllTasks', { tasks: data });
+    });
 });
+
+app.get('/deleteTask', function (req, res) {
+    res.sendFile(__dirname + '/views/deleteTask.html');
+});
+
+app.post('/deleteTask', function (req, res) {
+    let taskDetails = req.body;
+    let filter = { _id: mongodb.ObjectID(taskDetails._id) };
+    db.collection('week6').deleteOne(filter);
+    res.redirect('/getAllTasks');
+});
+
+app.get('/deleteCompleted', function (req, res) {
+    let taskDetails = req.body;
+    let filter = { taskStatus: taskDetails.taskStatus ="Complete" };
+    db.collection('week6').deleteMany(filter);
+    res.redirect('/getAllTasks');
+});
+
+app.get('/updateTask', function (req, res) {
+    res.sendFile(__dirname + '/views/updateTask.html');
+});
+
+app.post('/updateTask', function (req, res) {
+    let taskDetails = req.body;
+    let filter = { _id: mongodb.ObjectID(taskDetails._id) };
+    let theUpdate = { $set: { taskStatus: taskDetails.taskStatus } };
+    db.collection('week6').updateOne(filter, theUpdate);
+    res.redirect('/getAllTasks');
+})
 
 app.listen(8080);
